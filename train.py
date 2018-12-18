0c66692def90a23e584f5ea1316ba3b3bcd51ddf
Shinichiro Tamiya <tamiya@bluemoon.slp.cs.tut.ac.jp>
2018-12-18 22:13:09 +0900

    add train_recognizer

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 from torch.autograd import Variable
 from torchvision import transforms
+from torchvision import models, datasets
 from dataset import TuneDataset
 from model import Recognizer
 
@@ -35,9 +36,52 @@ def train_recognizer(args):
     train_loader = data_util.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
     valid_loader = data_util.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
     loaders = {"train":train_loader, "valid":valid_loader}
+    dataset_size = {"train":len(train_dataset), "valid":len(valid_dataset)}
     if args.gpu:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     else:
         device = torch.device("cpu")
 
     recognizer = Recognizer(args)
+    optimizer = optim.Adam(recognizer.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    best_model_wts = recognizer.state_dict()
+    best_loss = 1e10
+    
+    criterion = nn.BCELoss(reduction="sum")
+    start_time = time.time()
+
+    for epoch in range(args.epoch):
+        print("epoch {}".format(epoch+1))
+
+        for phase in ["train", "valid"]:
+            if phase == "train":
+                recognizer.train(True)
+            else:
+                recognizer.train(False)
+            
+            running_loss = 0.0
+            for i, data in enumerate(loaders[phase]):
+                inputs, label = data
+                inputs = Variable(inputs).to(device)
+                label = Variable(label).to(device)
+                if phase == "train":
+                    torch.set_grad_enabled(True)
+                else:
+                    torch.set_grad_enabled(False)
+                optimizer.zero_grad()
+                pred = recognizer(inputs)
+                loss = criterion(pred, label)
+                if phase == "train":
+                    loss.backward()
+                    optimizer.step()
+                running_loss += loss.item
+                epoch_loss = running_loss / dataset_size[phase] * args.batch_size
+                print("{} loss {:5f}".format(phase, epoch_loss))
+                if phase == "valid" and epoch_loss < best_loss :
+                    best_model_wts = recognizer.state_dict()
+                    best_loss = epoch_loss
+    elapsed_time = time.time() - start_time
+    print("training complete in {:0}s".format(elapsed_time))
+    recognizer.load_state_dict(best_model_wts)
+    return recognizer
+
